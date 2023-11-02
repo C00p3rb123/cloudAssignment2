06d5b893ed288dbd53b1927539c279ee237e215e
pondpiu <peerawas.a@gmail.com>
2023-11-02 15:32:24 +1000

    credential modal

--- client/src/components/Modal.tsx
+++ client/src/components/Modal.tsx
@@ -1,14 +1,13 @@
 import { Dialog, Transition } from "@headlessui/react";
 import { Fragment } from "react";
-import { trpc } from "../utils/trpc";
 
 type Props = {
   isOpen: boolean;
   onClose: () => void;
-  bookId: string;
+  serviceName: string | undefined;
 };
 
-export const Modal = ({ isOpen, onClose, bookId }: Props) => {
+export const Modal = ({ isOpen, onClose, serviceName }: Props) => {
   return (
     <Transition.Root show={isOpen} as={Fragment}>
       <Dialog as="div" className="relative z-10" onClose={onClose}>
@@ -25,27 +24,28 @@ export const Modal = ({ isOpen, onClose, bookId }: Props) => {
         </Transition.Child>
 
         <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div className="flex min-h-full justify-center text-center items-center p-0">
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
-              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              enterFrom="opacity-0 translate-y-0 scale-95"
+              enterTo="opacity-100 translate-y-0 scale-100"
               leave="ease-in duration-200"
-              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              leaveFrom="opacity-100 translate-y-0 scale-100"
+              leaveTo="opacity-0 translate-y-0 scale-95"
             >
               <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                  <div className="sm:flex sm:items-start">
-                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                <div className="bg-white  p-6 pb-4">
+                  <div className="flex items-start">
+                    <div className="ml-4 mt-0 text-left">
                       <Dialog.Title
                         as="h3"
                         className="text-base font-semibold leading-6 text-gray-900"
                       >
-                        Title
+                        {serviceName}
                       </Dialog.Title>
-                      <div className="mt-2">Body</div>
+                      <div className="mt-2">Email : {"<Email>"}</div>
+                      <div className="mt-2">Password : {"<Password>"}</div>
                     </div>
                   </div>
                 </div>
