1170c9888693e6b183733126289224256e271c09
pondpiu <peerawas.a@gmail.com>
2023-11-04 11:16:54 +1000

    register button

--- client/src/components/LoginPage.tsx
+++ client/src/components/LoginPage.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { useAuth } from "../provider/AuthProvider";
 import { useNavigate } from "react-router-dom";
 import { API_URL } from "../config";
@@ -29,6 +29,24 @@ export const LoginPage = () => {
       updateToken(token);
       navigate("/account", { replace: true });
     } catch (error) {
+      if (error instanceof AxiosError) {
+        alert(error.message);
+      }
+      console.error(error);
+    }
+  };
+
+  const handleRegister = async () => {
+    try {
+      const response = await axios.post(`${API_URL}/account/create-account`, {
+        email,
+        password,
+      });
+      alert(response.data.message);
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        alert(error.message);
+      }
       console.error(error);
     }
   };
@@ -67,6 +85,13 @@ export const LoginPage = () => {
             className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
           >
             Login
+          </button>
+          <button
+            type="button"
+            onClick={handleRegister}
+            className="bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
+          >
+            Register
           </button>
         </form>
       </div>
